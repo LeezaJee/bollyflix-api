274795b8ff0cb856fe89f5c458dcffdfe29b2981
Mona Lisa <lisa.nadeem@outlook.de>
2022-05-05 00:56:03 +0200

    added error handling middleware function

--- index.js
+++ index.js
@@ -161,6 +161,11 @@ app.get("/movies", (req, res) => {
 
 app.use(express.static("public")); //serves “documentation.html” file from the public folder
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080");
 });
